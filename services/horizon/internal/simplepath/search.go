danielnapierski/go

commit f165c3ed89885b6a2a2ed97f50fc77dd9a9573b3

Fix and improve path finding (#719)
This commit fixes several bugs and adds improvements making path finding
results better, returning them in a shorter time.

The most severe bug was found in `pathNode.Cost` method. `pathNode` is a
linked list node with `Tail` field pointing to the next asset on the
path. So the front of the list is the currently extended source asset
and the tail is always the destination asset. The previous code was
wrongly consuming offers in the wrong order. Fixing this bug required to
walk through the current path from the tail (the destination asset) to
front (the current source).

I added a new test explaining why the old code was wrong. Consider the
path: `AAA -> BBB -> CCC`. The core of the problem is that the previous
code was using `DestinationAmount` as a starting value for `AAA -> BBB`
trade, in other words: "give me amount of `AAA` needed to buy
`DestinationAmount` of `BBB`". It doesn't make any sense. What you
should do first is: "give me amount of `BBB` needed to buy
`DestinationAmount` of `CCC`". So for the destination amount equal `10`
and the following offers (`scc` notation):
```
offer :trader, {for:["AAA", :gateway], sell:["BBB", :gateway]}, 1, 11
offer :trader, {for:["BBB", :gateway], sell:["CCC", :gateway]}, 10, 0.1
```
The old algorithm would not find a path as it is not possible to buy
`1 BBB` with `10 AAA` and `1 BBB` is needed to buy `10 CCC`.

Fixing this introduced a huge optimization opportunity. Basically the
cost of trades between current source asset and the destination asset
does not change after prepending the path with a new asset. So we can
cache the results instead of checking every order book on the path every
time we add a new asset in the front of the path.

Another improvement was to remove `A -> B -> A` subpaths from the paths
as they will always be a bad deal. This also removed all `destination
asset -> A -> destination asset` paths.

services/horizon/internal/simplepath/search.go
@@ -1,9 +1,14 @@
 package simplepath
 
 import (
+	"github.com/stellar/go/services/horizon/internal/db2/core"
 	"github.com/stellar/go/services/horizon/internal/paths"
 	"github.com/stellar/go/xdr"
 )
+
+// MaxPathLength is a maximum path length as defined in XDR file (includes source and
+// destination assets).
+const MaxPathLength uint = 7
 
 // search represents a single query against the simple finder.  It provides
 // a place to store the results of the query, mostly for the purposes of code
@@ -16,14 +21,14 @@ import (
 // 3.  Call Run() to perform the search.
 //
 type search struct {
-	Query  paths.Query
-	Finder *Finder
+	Query     paths.Query
+	Q         *core.Q
+	MaxLength uint
 
 	// Fields below are initialized by a call to Init() after
 	// setting the fields above
 	queue   []computedNode
 	targets map[string]bool
-	visited map[string]bool
 
 	//This fields below are initialized after the search is run
 	Err     error
@@ -53,9 +58,11 @@ func (s *search) Init() {
 	p0 := pathNode{
 		Asset: s.Query.DestinationAsset,
 		Tail:  nil,
-		Q:     s.Finder.Q,
+		Q:     s.Q,
+		Depth: 1,
 	}
 	var c0 xdr.Int64
+	// `Cost` on destination node does not use DB connection.
 	c0, s.Err = p0.Cost(s.Query.DestinationAmount)
 	if s.Err != nil {
 		return
@@ -76,7 +83,6 @@ func (s *search) Init() {
 		s.targets[a.String()] = true
 	}
 
-	s.visited = map[string]bool{}
 	s.Err = nil
 	s.Results = nil
 }
@@ -87,6 +93,22 @@ func (s *search) Run() {
 	if s.Err != nil {
 		return
 	}
+
+	// db.Session does not provide a way to start a transaction with a
+	// specific isolation level. We need REPEATABLE READ here to have a
+	// stable view of the offers table. Without it, it's possible that
+	// search started in ledger X and finished in ledger X+1 would give
+	// invalid results.
+	//
+	// https://www.postgresql.org/docs/9.1/static/transaction-iso.html
+	// > Note that only updating transactions might need to be retried;
+	// > read-only transactions will never have serialization conflicts.
+	_, s.Err = s.Q.ExecRaw("BEGIN ISOLATION LEVEL REPEATABLE READ, READ ONLY")
+	if s.Err != nil {
+		return
+	}
+
+	defer s.Q.ExecRaw("ROLLBACK")
 
 	for s.hasMore() {
 		s.runOnce()
@@ -120,17 +142,6 @@ func (s *search) isTarget(id string) bool {
 	return found
 }
 
-// shouldVisit returns true if the asset id provided has not been
-// visited on this search, after marking the id as visited
-func (s *search) shouldVisit(id string) bool {
-	if _, found := s.visited[id]; found {
-		return false
-	}
-
-	s.visited[id] = true
-	return true
-}
-
 // runOnce processes the head of the search queue, findings results
 // and extending the search as necessary.
 func (s *search) runOnce() {
@@ -141,14 +152,7 @@ func (s *search) runOnce() {
 		s.Results = append(s.Results, cur.asPath())
 	}
 
-	if !s.shouldVisit(id) {
-		return
-	}
-
-	// A PathPaymentOp's path cannot be over 5 elements in length, and so
-	// we abort our search if the current linked list is over 7 (since the list
-	// includes both source and destination in addition to the path)
-	if cur.path.Depth() > 7 {
+	if cur.path.Depth == s.MaxLength {
 		return
 	}
 
@@ -158,16 +162,32 @@ func (s *search) runOnce() {
 func (s *search) extendSearch(p pathNode) {
 	// find connected assets
 	var connected []xdr.Asset
-	s.Err = s.Finder.Q.ConnectedAssets(&connected, p.Asset)
+	s.Err = s.Q.ConnectedAssets(&connected, p.Asset)
 	if s.Err != nil {
 		return
 	}
 
 	for _, a := range connected {
+		// If asset already exists on the path, continue to the next one.
+		// We don't want the same asset on the path twice as buying and
+		// then selling the asset will be a bad deal in most cases
+		// (especially A -> B -> A trades).
+		if p.IsOnPath(a) {
+			continue
+		}
+
+		// If the connected asset is not our target and the current length
+		// of the path is MaxLength-1 then it does not make sense to extend
+		// such path.
+		if p.Depth == s.MaxLength-1 && !s.isTarget(a.String()) {
+			continue
+		}
+
 		newPath := pathNode{
 			Asset: a,
 			Tail:  &p,
-			Q:     s.Finder.Q,
+			Q:     s.Q,
+			Depth: p.Depth + 1,
 		}
 
 		var hasEnough bool

services/horizon/internal/actions/helpers.go
@@ -237,11 +237,29 @@ func (base *Base) GetAccountID(name string) (result xdr.AccountId) {
 // conventions
 func (base *Base) GetAmount(name string) (result xdr.Int64) {
 	var err error
-	result, err = amount.Parse(base.GetString("destination_amount"))
+	result, err = amount.Parse(base.GetString(name))
 
 	if err != nil {
 		base.SetInvalidField(name, err)
 		return
+	}
+
+	return
+}
+
+// GetPositiveAmount returns a native amount (i.e. 64-bit integer) by parsing
+// the string at the provided name in accordance with the stellar client
+// conventions. Renders error for negative amounts and zero.
+func (base *Base) GetPositiveAmount(name string) (result xdr.Int64) {
+	result = base.GetAmount(name)
+
+	if base.Err != nil {
+		return
+	}
+
+	if result <= 0 {
+		base.SetInvalidField(name, errors.New("Value must be positive"))
+		return xdr.Int64(0)
 	}
 
 	return

services/horizon/internal/actions/helpers_test.go
@@ -179,6 +179,54 @@ func TestGetInt64(t *testing.T) {
 	tt.Assert.Equal(int64(math.MinInt64), result)
 }
 
+func TestAmount(t *testing.T) {
+	tt := test.Start(t)
+	defer tt.Finish()
+	action := makeTestAction()
+
+	result := action.GetAmount("minus_one")
+	tt.Assert.NoError(action.Err)
+	tt.Assert.Equal(xdr.Int64(-10000000), result)
+
+	result = action.GetAmount("zero")
+	tt.Assert.NoError(action.Err)
+	tt.Assert.Equal(xdr.Int64(0), result)
+
+	result = action.GetAmount("two")
+	tt.Assert.NoError(action.Err)
+	tt.Assert.Equal(xdr.Int64(20000000), result)
+
+	result = action.GetAmount("twenty")
+	tt.Assert.NoError(action.Err)
+	tt.Assert.Equal(xdr.Int64(200000000), result)
+}
+
+func TestPositiveAmount(t *testing.T) {
+	tt := test.Start(t)
+	defer tt.Finish()
+	action := makeTestAction()
+
+	result := action.GetPositiveAmount("minus_one")
+	tt.Assert.Error(action.Err)
+	tt.Assert.Equal(xdr.Int64(0), result)
+	action.Err = nil
+
+	result = action.GetPositiveAmount("zero")
+	tt.Assert.Error(action.Err)
+	tt.Assert.Equal(xdr.Int64(0), result)
+	action.Err = nil
+
+	result = action.GetPositiveAmount("two")
+	tt.Assert.NoError(action.Err)
+	tt.Assert.Equal(xdr.Int64(20000000), result)
+	action.Err = nil
+
+	result = action.GetPositiveAmount("twenty")
+	tt.Assert.NoError(action.Err)
+	tt.Assert.Equal(xdr.Int64(200000000), result)
+	action.Err = nil
+}
+
 func TestGetLimit(t *testing.T) {
 	tt := test.Start(t)
 	defer tt.Finish()
@@ -310,8 +358,10 @@ func makeAction(path string, body map[string]string) *Base {
 func testURLParams() map[string]string {
 	return map[string]string{
 		"blank":                "",
+		"minus_one":            "-1",
 		"zero":                 "0",
 		"two":                  "2",
+		"twenty":               "20",
 		"32min":                fmt.Sprint(math.MinInt32),
 		"32max":                fmt.Sprint(math.MaxInt32),
 		"64min":                fmt.Sprint(math.MinInt64),

services/horizon/internal/actions_path.go
@@ -29,7 +29,7 @@ func (action *PathIndexAction) JSON() {
 }
 
 func (action *PathIndexAction) loadQuery() {
-	action.Query.DestinationAmount = action.GetAmount("destination_amount")
+	action.Query.DestinationAmount = action.GetPositiveAmount("destination_amount")
 	action.Query.DestinationAddress = action.GetAddress("destination_account")
 	action.Query.DestinationAsset = action.GetAsset("destination_")
 }
@@ -46,7 +46,7 @@ func (action *PathIndexAction) loadSourceAssets() {
 }
 
 func (action *PathIndexAction) loadRecords() {
-	action.Records, action.Err = action.App.paths.Find(action.Query)
+	action.Records, action.Err = action.App.paths.Find(action.Query, action.App.config.MaxPathLength)
 }
 
 func (action *PathIndexAction) loadPage() {

services/horizon/internal/config.go
@@ -22,6 +22,8 @@ type Config struct {
 	SentryDSN              string
 	LogglyTag              string
 	LogglyToken            string
+	// Maximum length of the path returned by `/paths` endpoint.
+	MaxPathLength uint
 	// TLSCert is a path to a certificate file to use for horizon's TLS config
 	TLSCert string
 	// TLSKey is the path to a private key file to use for horizon's TLS config

services/horizon/internal/paths/main.go
@@ -23,5 +23,6 @@ type Path struct {
 
 // Finder finds paths.
 type Finder interface {
-	Find(Query) ([]Path, error)
+	// Returns path for a Query of a maximum length `maxLength`
+	Find(q Query, maxLength uint) ([]Path, error)
 }

services/horizon/internal/simplepath/doc.go
@@ -1,3 +1,38 @@
-// Package simplepath provides an implementation of paths.Finder that performs
-// a breadth first search for paths against a stellar-core's database
+// Package simplepath provides an implementation of paths. Finder that performs
+// a breadth first search for paths against a stellar-core's database.
+//
+// The core algorithm works as follows:
+// 1. `search` object contains a queue of currently extended paths. Queue is
+//    initialized with a single-asset path containing destination asset. Paths
+//    are linked lists, head of the path is a newly extended source asset, tail
+//    of the path is always the destination asset.
+// 2. Every iteration of search does the following:
+//    - pops a path from the queue,
+//    - checks if the path starts with a source asset, if it does, it appends
+//      the path to results,
+//    - finds all assets connected to the head of the current path and prepends
+//      the path, calculating the current cost.
+// Algorithm ends when there is no more paths to extend (len(queue) = 0) or
+// `maxResults` has been reached.
+//
+// The actual calculation of the cost is happening in `pathNode.Cost()` method.
+// There are a couple of important things to note:
+// 1. We are given `DestinationAmount` and the destination asset is the tail of
+// the list. So we need to start from the end of the path and continue to the
+// front.
+// 2. Because we are going from the tail to the head of the path, given the path
+// A -> B -> C. we are interested in finding:
+//    - amount of B needed to buy `DestinationAmount` of C,
+//    - amount of A needed to by above amount of B.
+// 3. Finally, the actual path payment will sell A, buy B, sell B, buy C. So we
+// need to check the following orderbooks:
+//    - sell C, buy B (the user will sell B, buy C),
+//    - sell B, buy A (the user will sell A, buy B).
+//
+// The algorithm works as follows:
+// 1. Because the head of the path is source account, we build a stack of assets
+//    to reverse that order.
+// 2. We start with the last asset (pop the stack), calculate it's cost (if not
+//    cached) and continue towards the source asset (bottom of the stack).
+// 3. We return the final cost.
 package simplepath

services/horizon/internal/simplepath/finder.go
@@ -20,7 +20,7 @@ type Finder struct {
 var _ paths.Finder = &Finder{}
 
 // Find performs a path find with the provided query.
-func (f *Finder) Find(q paths.Query) (result []paths.Path, err error) {
+func (f *Finder) Find(q paths.Query, maxLength uint) (result []paths.Path, err error) {
 	log.WithField("source_assets", q.SourceAssets).
 		WithField("destination_asset", q.DestinationAsset).
 		WithField("destination_amount", q.DestinationAmount).
@@ -31,9 +31,19 @@ func (f *Finder) Find(q paths.Query) (result []paths.Path, err error) {
 		return
 	}
 
+	if maxLength == 0 {
+		maxLength = MaxPathLength
+	}
+
+	if maxLength < 2 || maxLength > MaxPathLength {
+		err = errors.New("invalid value of maxLength")
+		return
+	}
+
 	s := &search{
-		Query:  q,
-		Finder: f,
+		Query:     q,
+		Q:         &core.Q{f.Q.Clone()},
+		MaxLength: maxLength,
 	}
 
 	s.Init()

commit 480176be6c1350dc32f726808f2eb14268c82530

Fix transaction bug introduced in #719

services/horizon/internal/simplepath/search.go
@@ -94,21 +94,24 @@ func (s *search) Run() {
 		return
 	}
 
-	// db.Session does not provide a way to start a transaction with a
-	// specific isolation level. We need REPEATABLE READ here to have a
-	// stable view of the offers table. Without it, it's possible that
-	// search started in ledger X and finished in ledger X+1 would give
-	// invalid results.
-	//
-	// https://www.postgresql.org/docs/9.1/static/transaction-iso.html
-	// > Note that only updating transactions might need to be retried;
-	// > read-only transactions will never have serialization conflicts.
-	_, s.Err = s.Q.ExecRaw("BEGIN ISOLATION LEVEL REPEATABLE READ, READ ONLY")
+	s.Err = s.Q.Begin()
 	if s.Err != nil {
 		return
 	}
 
-	defer s.Q.ExecRaw("ROLLBACK")
+	defer s.Q.Rollback()
+
+	// We need REPEATABLE READ here to have a stable view of the offers
+	// table. Without it, it's possible that search started in ledger X
+	// and finished in ledger X+1 would give invalid results.
+	//
+	// https://www.postgresql.org/docs/9.1/static/transaction-iso.html
+	// > Note that only updating transactions might need to be retried;
+	// > read-only transactions will never have serialization conflicts.
+	_, s.Err = s.Q.ExecRaw("SET TRANSACTION ISOLATION LEVEL REPEATABLE READ, READ ONLY")
+	if s.Err != nil {
+		return
+	}
 
 	for s.hasMore() {
 		s.runOnce()